6cd2e7dda1d360fcbe8117e16651651573631678
kyogoku01234 <41919648+kyogoku01234@users.noreply.github.com>
2018-12-29 13:51:06 +0700

    Commit Code
    Commit Code

---
+++ VnpApp/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace VnpApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.lblUserFind = new System.Windows.Forms.Label();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.btnSaveUser = new System.Windows.Forms.Button();
+            this.cboUserFind = new System.Windows.Forms.ListBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser3 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(15, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(651, 214);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 242);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(53, 13);
+            this.lblName.TabIndex = 13;
+            this.lblName.Text = "Thời Gian";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(71, 239);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(100, 20);
+            this.txtTime.TabIndex = 14;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(431, 237);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 19;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(305, 237);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(100, 23);
+            this.btnFind.TabIndex = 18;
+            this.btnFind.Text = "Tra Trong Kho";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.Location = new System.Drawing.Point(200, 238);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(75, 23);
+            this.btnHome.TabIndex = 17;
+            this.btnHome.Text = "Trang Chủ";
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // lblUserFind
+            // 
+            this.lblUserFind.AutoSize = true;
+            this.lblUserFind.Location = new System.Drawing.Point(554, 246);
+            this.lblUserFind.Name = "lblUserFind";
+            this.lblUserFind.Size = new System.Drawing.Size(0, 13);
+            this.lblUserFind.TabIndex = 20;
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Location = new System.Drawing.Point(12, 265);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(654, 250);
+            this.webBrowser2.TabIndex = 21;
+            // 
+            // btnSaveUser
+            // 
+            this.btnSaveUser.Location = new System.Drawing.Point(700, 90);
+            this.btnSaveUser.Name = "btnSaveUser";
+            this.btnSaveUser.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveUser.TabIndex = 26;
+            this.btnSaveUser.Text = "Save user";
+            this.btnSaveUser.UseVisualStyleBackColor = true;
+            this.btnSaveUser.Click += new System.EventHandler(this.btnSaveUser_Click);
+            // 
+            // cboUserFind
+            // 
+            this.cboUserFind.FormattingEnabled = true;
+            this.cboUserFind.Location = new System.Drawing.Point(690, 131);
+            this.cboUserFind.Name = "cboUserFind";
+            this.cboUserFind.Size = new System.Drawing.Size(100, 381);
+            this.cboUserFind.TabIndex = 25;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(700, 56);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 24;
+            this.btnLogin.Text = "login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(690, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 23;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(690, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 22;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // webBrowser3
+            // 
+            this.webBrowser3.Location = new System.Drawing.Point(796, 231);
+            this.webBrowser3.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser3.Name = "webBrowser3";
+            this.webBrowser3.Size = new System.Drawing.Size(456, 281);
+            this.webBrowser3.TabIndex = 27;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 528);
+            this.Controls.Add(this.webBrowser3);
+            this.Controls.Add(this.btnSaveUser);
+            this.Controls.Add(this.cboUserFind);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.lblUserFind);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Label lblUserFind;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+        private System.Windows.Forms.Button btnSaveUser;
+        private System.Windows.Forms.ListBox cboUserFind;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser3;
+    }
+}
+
---
+++ VnpApp/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VnpApp
+{
+    public partial class Form1 : Form
+    {
+        private string UserName = string.Empty;
+
+        private string PassWord = string.Empty;
+        string stbed = string.Empty;
+        dynamic timer;
+        bool checkLoop = true;
+        int countLB = 0;
+        int indexUser = 0;
+        //  string ScriptGetNumber = "document.getElementById('lblKetqua').getElementsByTagName('tr')[1].getElementsByTagName('td')[1].innerText";
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.loadPage();
+            this.AddListUser();
+            LoadUser();
+        }
+
+        public void loadPage()
+        {
+            webBrowser1.Navigate("http://10.149.34.168/ccbs/login.htm");
+        }
+
+        private void LoadUser()
+        {
+            string[] User = new string[2];
+            if (File.Exists("./User/User.txt"))
+            {
+                User = File.ReadAllLines("./User/User.txt");
+            }
+            UserName = User[0];
+            PassWord = User[1];
+
+            textBox1.Text = UserName;
+            textBox2.Text = PassWord;
+
+            txtTime.Text = "7000";
+
+        }
+
+        public void AddListUser()
+        {
+            cboUserFind.Items.Add("tuyennt_hcm");
+            cboUserFind.Items.Add("dtt_phuongnth_hcm");
+            cboUserFind.Items.Add("ngocdq_hcm");
+            cboUserFind.Items.Add("trinnm_hcm");
+            cboUserFind.Items.Add("nhuttt_hcm");
+            cboUserFind.Items.Add("tripv_hcm");
+            cboUserFind.Items.Add("chinhnv_hcm");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Object[] objArray = new Object[2];
+            objArray[0] = "1";
+            objArray[1] = "10";
+            webBrowser2.Document.InvokeScript("layds_thuebao2", objArray);
+        }
+
+
+        private void LoadPageFindNumber()
+        {
+            webBrowser2.Navigate("http://10.149.34.168/ccbs/main?1iutlomLork=vzzh5rgvnj5inutyu5otjk~");
+        }
+
+        private void LoadPageRegister()
+        {
+            webBrowser3.Navigate("http://10.149.34.168/ccbs/main?1iutlomLork=VZZH5jqeinutyu%7Fk{ig{5otjk~");
+        }
+
+        public void login(string user, string pass)
+        {
+            var userElements = webBrowser1.Document.GetElementById("username");
+            var passElements = webBrowser1.Document.GetElementById("passWord");
+            var clickElements = webBrowser1.Document.GetElementById("btnLogin");
+            userElements.InnerText = user;
+            passElements.InnerText = pass;
+            clickElements.InvokeMember("click");
+
+
+            while (webBrowser1.Document.GetElementById("ab") == null)
+            {
+                Application.DoEvents();
+                Thread.Sleep(500);
+
+            }
+            this.LoadPageChild();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string user = textBox1.Text;
+            string pass = textBox2.Text;
+            login(user, pass);
+
+        }
+
+        private void AddScriptFind(string name)
+        {
+            string script = "var Objects=new Array,Fields=new Array,pNum=1,pRec=10,iFlag=1;function form_load()" +
+             "{Objects=[\"txtSoTB\",\"cboLoaiSo\",\"cmdTimkiem\"],Fields=[\"SO_TB\",\"KIEU_SO\",null],change_subrange()," +
+             "check_flag()}function getDsKHoa(){DataRemoting.getDoc('neo.numstore.doc_layds_khoa(\"" + name + "\")',DOC_layds_khoa)}var DOC_layds_khoa=function(o){$(\"ajaxKhoa\").innerHTML=o};" +
+             "function check_flag(){DataRemoting.getValue(\"neo.numstore.doc_flag(5)\",flag_func)}" +
+             "var flag_func=function(o){iFlag=\"1\"==o?1:2};function layds_thuebao2(o,e){if(check_flag(),1==iFlag)" +
+             "if(1==$(\"cboTrangthai\").value)var a='neo.numstore.doc_layds_thuebao_dk(\"'+$(\"txtSoTB\").value+'\"," +
+             "\"'+$(\"cboLoaiSo\").value+'\",\"'+$(\"cboSubRange\").value+'\",\"'+$(\"cboTrangthai\").value+'\",\"" + name + "\",\"'+o+'\",\"'+e+'\")';else a='neo.numstore.doc_layds_thuebao_1(\"'+$(\"txtSoTB\").value+'\"," +
+             "\"'+$(\"cboLoaiSo\").value+'\",\"'+$(\"cboSubRange\").value+'\",\"'+$(\"cboTrangthai\").value+'\",\"" + name + "\",\"'+o+'\",\"'+e+'\")';else a='neo.numstore.doc_layds_thuebao_api(\"'+$(\"txtSoTB\").value+'\"," +
+             "\"'+$(\"cboLoaiSo\").value+'\",\"'+$(\"cboSubRange\").value+'\",\"'+$(\"cboTrangthai\").value+'\",\"" + name + "\"" +
+             ",\"'+o+'\",\"'+e+'\")';pNum=o,pRec=e,$(\"ajaxKetqua\").style.height=0,DataRemoting.getDoc(a,DOC_layds_thuebao)," +
+             "$(\"lblKetqua\").innerHTML=\"<font color=red><b>&#272;ang th&#7921;c hi&#7879;n ...</b></font>\"}" +
+             "var DOC_layds_thuebao=function(o){if($(\"ajaxKetqua\").style.height=0,$(\"ajaxKetqua\").innerHTML=o,1==iFlag)" +
+             "var e='neo.numstore.doc_layth_thuebao(\"'+$(\"txtSoTB\").value+'\",\"'+$(\"cboLoaiSo\").value+'\"," +
+             "\"'+pNum+'\",\"'+pRec+'\")';else e='neo.numstore.doc_layth_thuebao_api(\"'+pNum+'\",\"'+pRec+'\")';" +
+             "DataRemoting.getDoc(e,DOC_layth_thuebao)},DOC_layth_thuebao=function(o){$(\"ajaxKetquaTH\").innerHTML=o," +
+             "$(\"lblKetqua\").innerHTML=\"\"};function foward_dangky(o,e,a,n,t,u,i)" +
+             "{0==i?window.location=\"/ccbs/main?1y\u007fyezksvrgzkelork=rg\u007fu{z5iihy5zvreiihy&1iutlomLork=rgvvnok" +
+             "{5otjk~&yuezh=\"+o+\"&zkteqok{yu=\"+Url.encode_1252(e)+\"&i{uiezuoznok{=\"+Url.encode_1252(a)+\"&zmey{j" +
+             "{tm=\"+Url.encode_1252(n)+\"&jgziui=\"+Url.encode_1252(t)+\"&notnzn{iens=\"+Url.encode_1252(u):" +
+             "alert(Url.decode_1252(\"Thu&#234; bao &#273;&#227; &#273;&#259;ng k&#253;\"))}function foward_thongtin(o,e)" +
+             "{1==e?(openWindow(\"frmthongtin\",\"/ccbs/main?1iutlomLork=jgtmq\u007f5lxsznutmzot&yuezh=\"+o,\"frmthongtin\",500," +
+             "150,\"menubar=no,toolbar=no,location=no,status=yes,scrollbars=no,resizable=yes\"),frmkmtc.focus()):" +
+             "alert(\"Thue bao dang trong kho! \")}function forward_chitiet_tintuc(o){" +
+             "window.location=\"/ccbs/main?1y\u007fyezksvrgzkelork=rg\u007fu{z5iihy5zvreiihy&1iutlomLork=zotz{i5otjk~&tk}e" +
+             "oj=\"+o}function change_subrange(){var o='neo.numstore.doc_layds_kieuso_new(\"'+$(\"cboSubRange\").value+'\")';" +
+             "DataRemoting.getDoc(o,function(o){$(\"kieuso_s\").innerHTML=o})}";
+
+            HtmlDocument doc = webBrowser2.Document;
+            HtmlElement head = doc.GetElementsByTagName("head")[0];
+            HtmlElement s = doc.CreateElement("script");
+            s.SetAttribute("text", script);
+            head.AppendChild(s);
+        }
+
+        private void ClickFind(int page)
+        {
+            Object[] objArray = new Object[2];
+            objArray[0] = page.ToString();
+            objArray[1] = "10";
+            webBrowser2.Document.InvokeScript("layds_thuebao2", objArray);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            checkLoop = false;
+            btnStop.Visible = false;
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            string user = textBox1.Text;
+            string pass = textBox2.Text;
+            login(user, pass);
+
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://10.149.34.168/ccbs/login.htm");
+        }
+
+        private void btnSaveUser_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("./User/User.txt", textBox1.Text + Environment.NewLine + textBox2.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void button4_Click_1(object sender, EventArgs e)
+        //{
+        //    var length = webBrowser2.Document.GetElementById("table_phieu").GetElementsByTagName("tr").Count;
+        //    string stb = string.Empty;
+        //    string status = string.Empty;
+        //    for (var i = 1; i < length; i++)
+        //    {
+        //        stb = webBrowser2.Document.GetElementById("table_phieu").GetElementsByTagName("tr")[i].GetElementsByTagName("td")[1].InnerText;
+        //        status = webBrowser2.Document.GetElementById("table_phieu").GetElementsByTagName("tr")[i].GetElementsByTagName("td")[2].InnerText;
+        //        if (status == "Trong kho")
+        //        {
+        //            webBrowser3.Document.GetElementById("txtsomay").InnerHtml = stb;
+
+        //        }
+
+        //    }
+        //}
+
+        private void LoadPageChild()
+        {
+            lblUserFind.Text = cboUserFind.Items[0].ToString();
+            this.LoadPageFindNumber();
+            this.LoadPageRegister();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            lblUserFind.Text = cboUserFind.Items[0].ToString();
+            this.LoadPageFindNumber();
+            this.LoadPageRegister();
+        }
+
+        public void TranNumber()
+        {
+            int waitLoad = 0;
+
+            while (webBrowser2.Document.GetElementById("lblKetqua").GetElementsByTagName("b").Count == 1)
+            {
+                waitLoad++;
+                Application.DoEvents();
+            }
+
+            try
+            {
+                stbed = string.Empty;
+                var length = webBrowser2.Document.GetElementById("table_phieu").GetElementsByTagName("tr").Count;
+                string stb = string.Empty;
+                string status = string.Empty;
+                for (var i = 1; i < length; i++)
+                {
+                    stb = webBrowser2.Document.GetElementById("table_phieu").GetElementsByTagName("tr")[i].GetElementsByTagName("td")[1].InnerText;
+                    status = webBrowser2.Document.GetElementById("table_phieu").GetElementsByTagName("tr")[i].GetElementsByTagName("td")[2].InnerText;
+                    if (status == "Trong kho")
+                    {
+                        webBrowser3.Document.GetElementById("txtsomay").InnerHtml = stb;
+                        stbed = stb;
+                        break;
+                    }
+                }
+
+                if (length == 11 && string.IsNullOrEmpty(stbed))
+                {
+                    Thread.Sleep(7000);
+                    countLB++;
+                    ClickFind(countLB);
+                    TranNumber();
+                }
+            }
+            catch { }
+
+        }
+        private void autoFind()
+        {
+
+            if (indexUser >= cboUserFind.Items.Count)
+            {
+                indexUser = 0;
+            }
+
+            AddScriptFind(cboUserFind.Items[indexUser].ToString());
+            lblUserFind.Text = cboUserFind.Items[indexUser].ToString();
+            Thread.Sleep(300);
+
+            countLB++;
+            ClickFind(countLB);
+
+            TranNumber();
+
+            indexUser++;
+        }
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            btnStop.Visible = true;
+            checkLoop = true;
+            while (checkLoop && string.IsNullOrEmpty(stbed))
+            {
+                countLB = 0;
+                autoFind();
+                int waitLoad = 0;
+                while (true)
+                {
+                    waitLoad++;
+                    Application.DoEvents();
+                    Thread.Sleep(1);
+                    if (waitLoad == 7000) { break; }
+                }
+
+            }
+
+            //  autoFind();
+
+
+        }
+    }
+}
